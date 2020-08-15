e0474471ca6bba5a7f3367ae4463ce651e0d3f2f
Oxiyass <58347796+Oxiyass@users.noreply.github.com>
2020-08-15 21:28:11 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,40 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MarkerColor                = { r = 102, g = 0, b = 102 }
+Config.EnablePlayerManagement     = true
+Config.EnableVaultManagement      = false
+Config.EnableSocietyOwnedVehicles = true
+Config.EnableHelicopters          = false
+Config.EnableMoneyWash            = false
+Config.MaxInService               = -1
+Config.Locale                     = 'fr'
+
+Config.AuthorizedVehicles = {
+	{ name = '5008',  label = 'Peugeot 5008 2018' },
+	{ name = 'espace3',  label = 'Renault Espace 3' },
+}
+
+Config.Zones = {
+
+    BossActions = {
+        Pos   = { x = -1053.683, y = -230.554, z = 43.020 },
+        Size  = { x = 1.5, y = 1.5, z = 1.0 },
+        Color = { r = 0, g = 100, b = 0 },
+        Type  = 1,
+    },
+	
+	--[[Cloakrooms = {
+		Pos = { x = -1068.600, y = -241.440, z = 38.833},
+		Size = { x = 1.5, y = 1.5, z = 1.0 },
+        	Color = { r = 204, g = 204, b = 0 },
+		Type = 27,
+	},
+	--]]
+
+	VehicleDeleters = {
+		Pos  = { x = -1663.39, y = 78.18, z = 63.48},
+		Size = { x = 3.0, y = 3.0, z = 3.0 },
+        	Color = { r = 255, g = 0, b = 0 },		
+		Type = 1
+	},		
+}
