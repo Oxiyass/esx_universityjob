0520826b10ad5361d7f2a26007e2a02d4acd76ec
Oxiyass <58347796+Oxiyass@users.noreply.github.com>
2020-08-15 21:31:45 +0200

    Update esx_universityjob.sql

--- esx_universityjob.sql
+++ esx_universityjob.sql
@@ -1,6 +1,6 @@
 SET @job_name = 'university';
 SET @society_name = 'society_university';
-SET @job_Name_Caps = 'university';
+SET @job_Name_Caps = 'Université';
 
 INSERT INTO `addon_account` (name, label, shared) VALUES
   (@society_name, @job_Name_Caps, 1);
@@ -17,5 +17,5 @@ INSERT INTO `jobs` (name, label, whitelisted) VALUES
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
   (@job_name, 0, 'prof', 'Professeur', 250, '{}', '{}'),
   (@job_name, 1, 'profsup', 'Professeur Supp', 350, '{}', '{}'),
-  (@job_name, 2, 'secretaire', 'Secr�taire', 400, '{}', '{}'),
+  (@job_name, 2, 'secretaire', 'Secrétaire', 400, '{}', '{}'),
   (@job_name, 3, 'boss', "Directeur", 450, '{}', '{}');
